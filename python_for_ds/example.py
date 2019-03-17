363b63664650d495964139cba12d75b25404e9ea
Sajini-Sadanand <sadanand.sajini@gmail.com>
2019-03-17 20:02:13 +0530

    Add files via upload

---
+++ python_for_ds/example.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+add = lambda num1,num2: num1 + num2
+
+
+# In[ ]:
+
+
+def multiply(num1,num2):
+    """This function multiplies two numbers"""
+    return num1*num2
+
